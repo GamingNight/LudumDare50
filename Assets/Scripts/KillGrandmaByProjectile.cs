7393fce4602162c8083496f5040792923031aac0
JulesPomme <julian.moreira.fr@gmail.com>
2022-04-06 21:46:34 +0200

    grandma's death

--- Assets/Scripts/KillGrandmaByProjectile.cs
+++ Assets/Scripts/KillGrandmaByProjectile.cs
@@ -14,11 +14,13 @@ public class KillGrandmaByProjectile : MonoBehaviour
     private bool destroying;
     private float initPitch;
     private float initVolume;
+    private bool deathAnimFinished;
 
     private void Start() {
         destroying = false;
         initPitch = source.pitch;
         initVolume = source.volume;
+        deathAnimFinished = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
@@ -27,20 +29,22 @@ public class KillGrandmaByProjectile : MonoBehaviour
             int clipIndex = Random.Range(0, deathClips.Length);
             audioContainerSO.AddSource(source);
             source.clip = deathClips[clipIndex];
-            source.Play();
             source.pitch = initPitch + Random.Range(-0.3f, 0.3f);
             source.volume = initVolume + Random.Range(-0.1f, 0.1f);
-            //foreach(SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>()) {
-            //    sr.enabled = false;
-            //}
+            source.Play();
             GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<GrandmaMovement>().enabled = false;
             destroying = true;
             animator.SetTrigger("Death");
         }
     }
 
+    public void DeathAnimFinished() {
+        deathAnimFinished = true;
+    }
+
     private void Update() {
-        if (destroying && !source.isPlaying) {
+        if (destroying && !source.isPlaying && deathAnimFinished) {
             audioContainerSO.RemoveSource(source);
             destroying = false;
             Destroy(gameObject, 2);
